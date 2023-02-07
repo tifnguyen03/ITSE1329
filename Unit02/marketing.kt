514aa024a3c5150d3d90b8b6aee0805bf9148caf
tifnguyen03 <101011627+tifnguyen03@users.noreply.github.com>
2023-02-07 10:33:55 -0600

    Create marketing.kt

---
+++ Unit02/marketing.kt
@@ -0,0 +1,16 @@
+/*
+* Tiffany Nguyen
+* marketing.kt
+* 02/07/2023
+* */
+
+fun main(){
+    var friendsCount = 400
+
+    if(friendsCount>500){
+        println("Adopt a Dog Today!")
+    }
+    else{
+        println("Buy Cat Food At 20% Off")
+    }
+}
