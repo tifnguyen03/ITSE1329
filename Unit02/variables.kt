7181cce945b8bbb8374a49db1ee46d6abdf0d6c8
tifnguyen03 <101011627+tifnguyen03@users.noreply.github.com>
2023-02-07 10:21:35 -0600

    Update variables.kt

--- Unit02/variables.kt
+++ Unit02/variables.kt
@@ -1,3 +1,9 @@
+/*
+* Tiffany Nguyen
+* variable.kt
+* 02/07/2023
+* */
+
 fun main(args: Array<String>) {
 
     var myAge = 19;
@@ -5,13 +11,14 @@ fun main(args: Array<String>) {
     averageAge = (19.0 + 30.0) / 2.0
     println(averageAge)
 
-    val testNumber = 54
+    val testNumber = 53
     val evenOdd = testNumber % 2
     println(evenOdd)
     // if testNumber is even, then testNumber prints 0.
     // If testNumber is odd, then testNumber prints 1.
 
     var answer = 0
+    println(answer)
 
     answer = ++answer
     println(answer)
