a9cf399b4d365252c3ea57977583f34776ee9522
tifnguyen03 <101011627+tifnguyen03@users.noreply.github.com>
2023-02-07 09:48:49 -0600

    adding first unit02 program

---
+++ Unit02/variables.kt
@@ -0,0 +1,28 @@
+fun main(args: Array<String>) {
+	
+	var myAge = 19;
+    var averageAge = 19.0
+    averageAge = (19.0 + 30.0) / 2.0
+    println(averageAge)
+    
+    val testNumber = 54    
+    val evenOdd = testNumber % 2
+    println(evenOdd)
+    // if testNumber is even, then testNumber prints 0. 
+    // If testNumber is odd, then testNumber prints 1.
+    
+    var answer = 0
+    
+    answer = ++answer
+    println(answer)
+    
+    answer = answer + 10
+    println(answer)
+    
+    answer = answer * 10
+    println(answer)
+    
+    answer = answer * 1000
+    println(answer)
+    
+}
