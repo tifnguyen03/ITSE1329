2b3776233e2f520529eaa21908b8a34add35593f
tifnguyen03 <101011627+tifnguyen03@users.noreply.github.com>
2023-02-09 11:48:04 -0600

    Add files via upload

---
+++ Unit02/marketing.kt
@@ -0,0 +1,16 @@
+/*
+* Tiffany Nguyen
+* 02.3 Advertising Conditional
+* 02/09/2023
+* */
+
+fun main(){
+    var friendsCount = 400
+
+    if(friendsCount>500){
+        println("Adopt a Dog Today!")
+    }
+    else{
+        println("Buy Cat Food At 20% Off")
+    }
+}
