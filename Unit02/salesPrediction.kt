bc0f4bca73bced5bdaa8def587490125ba336993
tifnguyen03 <101011627+tifnguyen03@users.noreply.github.com>
2023-02-09 11:57:29 -0600

    Add files via upload

---
+++ Unit02/salesPrediction.kt
@@ -0,0 +1,14 @@
+/*
+* Tiffany Nguyen
+* 02.6 Sales Prediction
+* 02/09/2023
+* */
+fun main(){
+    val percent = .62
+    var eastCoast = 4600000
+
+    var total = eastCoast * percent
+
+    println("East Coast would make $$total of total sales.")
+
+}
