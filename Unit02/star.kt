0afb3c24c6566c47a07ab7072284ae9c69994075
tifnguyen03 <101011627+tifnguyen03@users.noreply.github.com>
2023-02-09 11:54:15 -0600

    Add files via upload

---
+++ Unit02/star.kt
@@ -0,0 +1,14 @@
+/*
+* Tiffany Nguyen
+* star.kt
+* 02/07/2023
+* */
+fun main(){
+    println("   *")
+    println("  ***")
+    println(" *****")
+    println("*******")
+    println(" *****")
+    println("  ***")
+    println("   *")
+}
