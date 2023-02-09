4353dc71b94473b9635d7945ea7d70b4ab16b73c
tifnguyen03 <101011627+tifnguyen03@users.noreply.github.com>
2023-02-09 12:05:10 -0600

    Add files via upload

---
+++ Unit02/stringMani.kt
@@ -0,0 +1,15 @@
+/*
+* Tiffany Nguyen
+* stringMani.kt
+* 02/09/2023
+* */
+fun main(){
+
+    print("Enter the name of your favorite city : ")
+    val city = readLine()!!
+
+    println(city.length)
+    println(city.uppercase())
+    println(city.lowercase())
+    println(city[0])
+}
