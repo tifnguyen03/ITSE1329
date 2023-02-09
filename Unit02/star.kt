02fb73f9780bb6296b08087ecf9d1423aae182b9
tifnguyen03 <101011627+tifnguyen03@users.noreply.github.com>
2023-02-09 11:55:07 -0600

    Add files via upload

---
+++ Unit02/star.kt
@@ -0,0 +1,14 @@
+/*
+* Tiffany Nguyen
+* 02.5 Star Pattern
+* 02/09/2023
+* */
+fun main(){
+    println("   *")
+    println("  ***")
+    println(" *****")
+    println("*******")
+    println(" *****")
+    println("  ***")
+    println("   *")
+}
